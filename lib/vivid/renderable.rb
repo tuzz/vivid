e65675d7cd4edc1382fe95add5cb357fad9b4d81
Chris Patuzzo <chris@patuzzo.co.uk>
2019-03-07 16:41:58 +0000

    Add support for custom arguments to .render
    This is needed for the Texture directive, as well
    as directives that take plain arguments such as
    Translate and Rotate.

--- lib/vivid/renderable.rb
+++ lib/vivid/renderable.rb
@@ -5,13 +5,36 @@ module Vivid
     end
 
     def to_pbrt(builder)
-      methods = self.class.render_names
-
-      chain = methods[0..-2].inject(builder) do |builder, name|
-        builder.public_send(name)
+      # [:foo, :@bar, :baz] -> [[:foo, :@bar], [:baz]]
+      chunks = self.class.render_names.chunk_while do |a, b|
+        b.to_s.start_with?("@")
       end
 
-      chain.public_send(methods.last, attributes || {})
+      # [[_, :@bar], _] -> [[_, :bar], _]
+      chunks = chunks.map do |method, *args|
+        [method, *args.map { |a| a.to_s[1..].to_sym }]
+      end
+
+      # Get the names of all arguments:
+      args = chunks.flat_map { |_, *args| args }
+
+      # [[_, :bar], _] -> [[_, "value"], _]
+      chunks = chunks.map do |method, *args|
+        [method, *args.map { |a| public_send(a) }]
+      end
+
+      # If the last method doesn't take a specified argument:
+      unless chunks.last.size > 1
+
+        # Set its argument to the remainder of the attributes hash:
+        remainder = (attributes || {}).reject { |k, _| args.include?(k) }
+        chunks.last.push(remainder)
+      end
+
+      # Call a chain of methods on the builder:
+      chunks.inject(builder) do |builder, (method, *args)|
+        builder.public_send(method, *args)
+      end
     end
 
     module ClassMethods
--- spec/lib/vivid/renderable_spec.rb
+++ spec/lib/vivid/renderable_spec.rb
@@ -39,5 +39,71 @@ module Vivid
     it "defines a method on the class to get the render names" do
       expect(RenderableTest.render_names).to eq [:shape, :sphere]
     end
+
+    class RenderableTest2
+      include Renderable
+
+      render :texture, :@name, :spectrum, :imagemap
+
+      def attributes
+        { name: name, filename: "foo.png" }
+      end
+
+      attr_accessor :name
+    end
+
+    it "can pass arguments to the chained methods" do
+      subject = RenderableTest2.new
+      builder = PBRT::Builder.new
+
+      subject.name = "mytexture"
+      subject.to_pbrt(builder)
+
+      expect(builder.to_s).to eq(<<~PBRT)
+        Texture "mytexture" "spectrum" "imagemap" "string filename" ["foo.png"]
+      PBRT
+    end
+
+    class RenderableTest3
+      include Renderable
+
+      render :translate, :@xy, :@z
+
+      attr_accessor :xy, :z
+    end
+
+    it "can pass arguments (instead of attributes) to the last method" do
+      subject = RenderableTest3.new
+      builder = PBRT::Builder.new
+
+      subject.xy = [1, 2]
+      subject.z = 3
+
+      subject.to_pbrt(builder)
+
+      expect(builder.to_s).to eq(<<~PBRT)
+        Translate 1 2 3
+      PBRT
+    end
+
+    class RenderableTest4
+      include Renderable
+
+      render :shape, :sphere, :@my_attributes
+
+      attr_accessor :my_attributes
+    end
+
+    it "can send a different set of attributes to the last method" do
+      subject = RenderableTest4.new
+      builder = PBRT::Builder.new
+
+      subject.my_attributes = { radius: 1 }
+      subject.to_pbrt(builder)
+
+      expect(builder.to_s).to eq(<<~PBRT)
+        Shape "sphere" "float radius" [1]
+      PBRT
+    end
   end
 end
