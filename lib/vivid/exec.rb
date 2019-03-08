140e91f5fbd3952ebd06d5b6f8ac6ce521a5019d
Chris Patuzzo <chris@patuzzo.co.uk>
2019-03-08 22:26:07 +0000

    Add a method that calls ffmpeg
    Since we’re already reading the frame_rate config
    in TransformTimes, I’d rather pass this value
    through than read it again from global config.
    
    Otherwise, the scene could change the frame rate
    and ffmpeg wouldn’t pick up on the change.

--- lib/vivid/exec.rb
+++ lib/vivid/exec.rb
@@ -3,5 +3,12 @@ module Vivid
     def self.pbrt(path:)
       system("pbrt #{Config.pbrt_flags} #{path}")
     end
+
+    def self.ffmpeg(input:, output:, rate:)
+      in_flags = Config.ffmpeg_in_flags
+      out_flags = Config.ffmpeg_out_flags
+
+      system("ffmpeg -r #{rate} #{in_flags} -i #{input} #{out_flags} #{output}")
+    end
   end
 end
