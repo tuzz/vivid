80edeb1f132e6ec8ac514f4f64f52abb9d9d53b6
Chris Patuzzo <chris@patuzzo.co.uk>
2019-03-12 14:59:28 +0000

    Render the shape associated with an AreaLight

--- app/lights/area_light.rb
+++ app/lights/area_light.rb
@@ -1,5 +1,15 @@
 class AreaLight < Vivid::Light
-  attributes :L, :twosided, :samples
+  attributes :shape, :L, :twosided, :samples
   render_as :area_light_source, :diffuse
   transforms :translate, :rotate, :scale
+  after_render :render_shape
+
+  def attributes
+    super.reject { |k, _| k == :shape }
+  end
+
+  def render_shape(builder)
+    shape.next_frame
+    shape.build_pbrt(builder)
+  end
 end
